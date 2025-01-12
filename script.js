7deca3ae093c232f972cc74c0a62be174429803b
serges123abobus <145282689+serges123abobus@users.noreply.github.com>
2025-01-12 12:08:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-document.getElementById('infoButton').addEventListener('click', function() {
-    document.getElementById('infoPopup').style.display = 'flex';
+// Обработчик события для кнопки открытия поп-апа
+document.getElementById('infoButton').addEventListener('click', () => {
+    const infoPopup = document.getElementById('infoPopup');
+    infoPopup.style.display = 'flex';
     hideImages(); // Скрываем изображения при открытии поп-апа
 });
 
-document.getElementById('closePopup').addEventListener('click', function() {
-    document.getElementById('infoPopup').style.display = 'none';
+// Обработчик события для кнопки закрытия поп-апа
+document.getElementById('closePopup').addEventListener('click', () => {
+    const infoPopup = document.getElementById('infoPopup');
+    infoPopup.style.display = 'none';
     showImages(); // Показываем изображения при закрытии поп-апа
 });
 
 // Закрытие поп-апа при клике вне его
-window.onclick = function(event) {
+window.addEventListener('click', (event) => {
     const popup = document.getElementById('infoPopup');
     if (event.target === popup) {
         popup.style.display = 'none';
         showImages(); // Показываем изображения при закрытии поп-апа
     }
-};
+});
 
+// Функция для скрытия изображений
 function hideImages() {
     const images = document.querySelectorAll('.product-card'); // Замените .image-class на класс ваших изображений
     images.forEach(image => {
         image.style.display = 'none'; // Скрываем изображения
     });
 }
-document.getElementById('shopButton').addEventListener('click', function() {
-    window.location.href = 'pravila.html';
-});
+
+// Функция для показа изображений
+function showImages() {
+    const images = document.querySelectorAll('.product-card'); // Замените .image-class на класс ваших изображений
+    images.forEach(image => {
+        image.style.display = 'block'; // Показываем изображения
+    });
+}
